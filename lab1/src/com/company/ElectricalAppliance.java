b0a71c2379b2bfd748d78c2818a87c1ef2766819
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-10-31 19:04:09 +0200

    Update the ElectricalAppliance class

--- lab1/src/com/company/ElectricalAppliance.java
+++ lab1/src/com/company/ElectricalAppliance.java
@@ -4,12 +4,37 @@ public abstract class ElectricalAppliance {
     // electric power (watts)
     private int power;
 
+    private boolean isConnectToElectricitySupply = false;
+
+    // status of electrical equipment (is working or is not working)
+    private boolean isWorking = false;
+
     public ElectricalAppliance(int power) {
         this.power = power;
     }
 
     // to connect a piece of electrical equipment to an electricity supply
-    public abstract boolean plug();
+    public void plug() {
+        isConnectToElectricitySupply = true;
+    }
+
+    // to stop a piece of electrical equipment being connected to an electricity supply
+    public void unplug() {
+        isConnectToElectricitySupply = false;
+        isWorking = false;
+    }
+
+    // to start a piece of electrical equipment working
+    public void turnOn() {
+        if (isConnectToElectricitySupply) {
+            isWorking = true;
+        }
+    }
+
+    // to stop a piece of electrical equipment working
+    public void turnOff() {
+        isWorking = false;
+    }
 
     // to get the electric power (watts)
     public int getPower() {
