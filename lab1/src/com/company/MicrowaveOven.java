269f8055d87a352c6a9417bf5739036e4b52e249
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-13 01:47:04 +0200

    Update the MicrowaveOven class

--- lab1/src/com/company/MicrowaveOven.java
+++ lab1/src/com/company/MicrowaveOven.java
@@ -1,8 +1,28 @@
 package com.company;
 
 public class MicrowaveOven extends ElectricalAppliance {
-    
+    private boolean isClose = true;
+
     public MicrowaveOven(int power) {
         super(power);
+    }
+
+    public void open() {
+        isClose = false;
+    }
+
+    public void close() {
+        isClose = true;
+    }
+
+    @Override
+    public void turnOn() {
+        if (isClose) {
+            super.turnOn();
+        }
+    }
+
+    public boolean isClose() {
+        return isClose;
     }
 }
