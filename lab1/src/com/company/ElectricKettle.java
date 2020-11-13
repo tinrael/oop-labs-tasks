cf3290cb9108a18307225aa917ef16142b30e65d
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-13 02:48:02 +0200

    Update the ElectricKettle class

--- lab1/src/com/company/ElectricKettle.java
+++ lab1/src/com/company/ElectricKettle.java
@@ -1,8 +1,33 @@
 package com.company;
 
 public class ElectricKettle extends ElectricalAppliance {
+    private final double MIN_VOLUME; // litres
+    private final double MAX_VOLUME; // litres
 
-    public ElectricKettle(int power) {
+    private double currentVolume = 0.0; // litres
+
+    public ElectricKettle(int power, double minVolume, double maxVolume) {
         super(power);
+        this.MIN_VOLUME = minVolume;
+        this.MAX_VOLUME = maxVolume;
+    }
+
+    public void fillWithWater(double litres) {
+        currentVolume += litres;
+    }
+    
+    @Override
+    public void turnOn() {
+        if ((Double.compare(currentVolume, MIN_VOLUME) >= 0) && (Double.compare(currentVolume, MAX_VOLUME) <= 0)) {
+            super.turnOn();
+        }
+    }
+
+    public double getMinVolume() {
+        return this.MIN_VOLUME;
+    }
+
+    public double getMaxVolume() {
+        return this.MAX_VOLUME;
     }
 }
