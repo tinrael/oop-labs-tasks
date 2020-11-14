2ddba8002a4f06349c5890874a0680e229284bda
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-15 01:45:57 +0200

    Add test methods

--- lab1/tests/com/company/ElectricKettleTest.java
+++ lab1/tests/com/company/ElectricKettleTest.java
@@ -12,11 +12,101 @@ class ElectricKettleTest {
     private final ElectricKettle kettle = new ElectricKettle(POWER, MIN_VOLUME, MAX_VOLUME);
 
     @Test
-    void fillWithWater() {
-        double delta = 0.0001;
+    void instantiate() {
+        assertEquals(POWER, kettle.getPower());
+        assertFalse(kettle.isConnectToElectricitySupply());
+        assertFalse(kettle.isWorking());
+        assertEquals(MIN_VOLUME, kettle.getMinVolume());
+        assertEquals(MAX_VOLUME, kettle.getMaxVolume());
+        assertEquals(0.0, kettle.getCurrentVolume());
+    }
 
-        assertEquals(0.0, kettle.getCurrentVolume(), delta);
-        kettle.fillWithWater(3.5);
-        assertEquals(3.5, kettle.getCurrentVolume(), delta);
+    @Test
+    void plug() {
+        kettle.plug();
+        assertTrue(kettle.isConnectToElectricitySupply());
+    }
+
+    @Test
+    void unplug() {
+        kettle.plug();
+        kettle.empty();
+        kettle.fill(MIN_VOLUME);
+        kettle.turnOn();
+
+        kettle.unplug();
+        assertFalse(kettle.isConnectToElectricitySupply());
+        assertFalse(kettle.isWorking());
+    }
+
+    @Test
+    void turnOn() {
+        kettle.turnOff();
+        kettle.unplug();
+        kettle.empty();
+
+        kettle.turnOn();
+        assertFalse(kettle.isWorking());
+        kettle.plug();
+        kettle.turnOn();
+        assertFalse(kettle.isWorking());
+        kettle.fill(MIN_VOLUME);
+        kettle.turnOn();
+        assertTrue(kettle.isWorking());
+        kettle.turnOff();
+        kettle.empty();
+        kettle.fill(MAX_VOLUME);
+        kettle.turnOn();
+        assertTrue(kettle.isWorking());
+        kettle.turnOff();
+        kettle.empty();
+        kettle.fill(MAX_VOLUME + 0.5);
+        kettle.turnOn();
+        assertFalse(kettle.isWorking());
+
+        ElectricKettle otherKettle = new ElectricKettle(17, 2.5, 7.0);
+        otherKettle.fill(2.0);
+        otherKettle.plug();
+        otherKettle.turnOn();
+        assertFalse(otherKettle.isWorking());
+        otherKettle.fill(0.5);
+        otherKettle.turnOn();
+        assertTrue(otherKettle.isWorking());
+        otherKettle.turnOff();
+        otherKettle.fill(3.0);
+        otherKettle.turnOn();
+        assertTrue(otherKettle.isWorking());
+    }
+
+    @Test
+    void turnOff() {
+        kettle.plug();
+        kettle.empty();
+        kettle.fill(MIN_VOLUME);
+        kettle.turnOn();
+
+        kettle.turnOff();
+        assertTrue(kettle.isConnectToElectricitySupply());
+        assertFalse(kettle.isWorking());
+    }
+
+    @Test
+    void fill() {
+        final double litres = 1.3;
+
+        kettle.empty();
+        kettle.fill(litres);
+        assertEquals(litres, kettle.getCurrentVolume());
+        kettle.fill(litres);
+        assertEquals(litres + litres, kettle.getCurrentVolume());
+        kettle.empty();
+        kettle.fill(-litres);
+        assertEquals(0.0, kettle.getCurrentVolume());
+    }
+
+    @Test
+    void empty() {
+        kettle.empty();
+        assertEquals(0.0, kettle.getCurrentVolume());
     }
 }
