360f23bfe3239016f1dddb47add06c4f2025e721
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-14 23:29:57 +0200

    Add some test methods

--- lab1/tests/com/company/MicrowaveOvenTest.java
+++ lab1/tests/com/company/MicrowaveOvenTest.java
@@ -10,16 +10,68 @@ class MicrowaveOvenTest {
     private final MicrowaveOven oven = new MicrowaveOven(POWER);
 
     @Test
-    void plug() {
+    void instantiate() {
+        assertEquals(POWER, oven.getPower());
         assertFalse(oven.isConnectToElectricitySupply());
+        assertFalse(oven.isWorking());
+        assertTrue(oven.isClose());
+    }
+
+    @Test
+    void plug() {
         oven.plug();
         assertTrue(oven.isConnectToElectricitySupply());
     }
 
     @Test
-    void open() {
+    void unplug() {
+        oven.plug();
+        oven.close();
+        oven.turnOn();
+
+        oven.unplug();
+        assertFalse(oven.isConnectToElectricitySupply());
+        assertFalse(oven.isWorking());
         assertTrue(oven.isClose());
+    }
+
+    @Test
+    void turnOn() {
+        oven.turnOff();
+        oven.unplug();
+        oven.open();
+
+        oven.turnOn();
+        assertFalse(oven.isWorking());
+        oven.plug();
+        oven.turnOn();
+        assertFalse(oven.isWorking());
+        oven.close();
+        oven.turnOn();
+        assertTrue(oven.isWorking());
+    }
+
+    @Test
+    void turnOff() {
+        oven.plug();
+        oven.close();
+        oven.turnOn();
+
+        oven.turnOff();
+        assertTrue(oven.isConnectToElectricitySupply());
+        assertFalse(oven.isWorking());
+        assertTrue(oven.isClose());
+    }
+
+    @Test
+    void open() {
         oven.open();
         assertFalse(oven.isClose());
+    }
+
+    @Test
+    void close() {
+        oven.close();
+        assertTrue(oven.isClose());
     }
 }
