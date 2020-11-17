8b8934d54d457256f92436bdcecce7166aeee314
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-18 00:35:19 +0200

    Update the Flat class

--- lab1/src/com/company/Flat.java
+++ lab1/src/com/company/Flat.java
@@ -1,12 +1,22 @@
 package com.company;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Flat {
-    private List<ElectricalAppliance> electricalAppliances = new LinkedList<>();
+    private HashMap<String, ElectricalAppliance> electricalAppliances = new LinkedHashMap<>();
 
-    public void addElectricalAppliance(ElectricalAppliance appliance) {
-        electricalAppliances.add(appliance);
+    public void addElectricalAppliance(String name, ElectricalAppliance electricalAppliance) {
+        electricalAppliances.put(name, electricalAppliance);
+    }
+
+    public int calculateTotalElectricityConsumption() {
+        int result = 0;
+        for (ElectricalAppliance electricalAppliance : electricalAppliances.values()) {
+            if (electricalAppliance.isConnectToElectricitySupply()) {
+                result += electricalAppliance.getPower();
+            }
+        }
+        return result;
     }
 }
