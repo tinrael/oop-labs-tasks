1e0fbcdcf4c03f8ea2a9b71e89684376373786ac
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-01-19 11:27:51 +0200

    Add the example of usage

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // System.out.println(XMLValidator.validate("data/planes.xml", "data/planes.xsd"));
+
+        PlanesBuilderFactory planesBuilderFactory = new PlanesBuilderFactory();
+        AbstractPlanesBuilder planesBuilder = planesBuilderFactory.createPlanesBuilder("stax");
+        planesBuilder.build("data/planes.xml");
+        System.out.println(planesBuilder.getPlanes());
     }
 }
